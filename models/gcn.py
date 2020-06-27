7220a8d0703ce159e91b267285fd998c4fbf8ccb
Wang Chen <jeffsan@gmx.com>
2020-06-27 14:07:58 -0400

    set bias to false

--- models/gcn.py
+++ models/gcn.py
@@ -6,25 +6,21 @@ import torch.nn as nn
 
 
 class GCN(nn.Module):
-    '''
-    GCN: Graph Convolutional Network, ICLR 2017
-    https://arxiv.org/pdf/1609.02907.pdf
-    '''
     def __init__(self, nfeat, nhid, nclass, dropout=0.5):
         super().__init__()
-        self.gc1 = GraphConv(nfeat, nhid)
-        self.gc2 = GraphConv(nhid, nclass)
+        self.gcn1 = GraphConv(nfeat, nhid)
+        self.gcn2 = GraphConv(nhid, nclass)
         self.acvt = nn.Sequential(nn.ReLU(), nn.Dropout(dropout))
         
     def forward(self, x, adj):
-        x = self.gc1(x, adj)
+        x = self.gcn1(x, adj)
         x = self.acvt(x)
-        x = self.gc2(x, adj)
+        x = self.gcn2(x, adj)
         return x
 
 
 class GraphConv(nn.Module):
-    def __init__(self, in_features, out_features, bias=True):
+    def __init__(self, in_features, out_features, bias=False):
         super().__init__()
         self.linear = nn.Linear(in_features, out_features, bias)
 

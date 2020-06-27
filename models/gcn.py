63cb9be76b5b08cb8fd6db232ce8b4e9cf9ecb28
Wang Chen <jeffsan@gmx.com>
2020-06-27 14:01:40 -0400

    add default dropout for gcn

--- models/gcn.py
+++ models/gcn.py
@@ -10,7 +10,7 @@ class GCN(nn.Module):
     GCN: Graph Convolutional Network, ICLR 2017
     https://arxiv.org/pdf/1609.02907.pdf
     '''
-    def __init__(self, nfeat, nhid, nclass, dropout):
+    def __init__(self, nfeat, nhid, nclass, dropout=0.5):
         super().__init__()
         self.gc1 = GraphConv(nfeat, nhid)
         self.gc2 = GraphConv(nhid, nclass)
